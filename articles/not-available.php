916a3a08f945a5f66dbe7fce3c1e753e1da2b59b
MandelOwO <87827886+MandelOwO@users.noreply.github.com>
2022-11-07 18:33:36 +0100

    not available page

---
+++ articles/not-available.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Článek není k dispozici</title>
+</head>
+<body>
+<?php
+$page = 'home';
+require_once '../source/pages/navbar.php';
+?>
+
+<h1>Vybraný článek není k dispozici</h1>
+</body>
+</html>
